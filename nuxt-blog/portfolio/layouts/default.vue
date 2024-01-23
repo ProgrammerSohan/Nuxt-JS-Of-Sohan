fa20fc694df0a075cbc123dbb59d95659f1d426a
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-24 05:39:46 +0600

    06_styling the app with tailwind css

--- nuxt-blog/portfolio/layouts/default.vue
+++ nuxt-blog/portfolio/layouts/default.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
-        <nav>
-            <ul>
-                <li>
-                    <NuxtLink to="/" class="text-blue-500">Main</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/about" class="text-blue-500">About</NuxtLink>
-                </li>
-                <li>
-                    <NuxtLink to="/sohan" class="text-blue-500">Sohan</NuxtLink>
-                </li>
-            </ul>
-        </nav>
-        <slot />
+    <div class="container mx-auto max-w-2xl">
+         <header class="flex justify-between items-center mt-5">
+               <div>
+                 <NuxtLink to="/sohan" class="text-xl font-semibold p-2 hover:bg-gray-200">Sohan Sujan</NuxtLink>
+               </div>
+            <nav class="font-mono">
+                <ul class="flex space-x-4">
+                    <li>
+                        <NuxtLink to="/" class="text-blue-500">Main</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/about" class="text-blue-500">About</NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink to="/sohan" class="text-blue-500">Sohan</NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+         </header>
+        <main class="p-2">
+            <slot />
+        </main>
     </div>
 </template>
