20065bacb504a6a7a44ac011e2744c066ca2806d
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-01-24 05:15:24 +0600

    05_tailwind added

--- nuxt-blog/portfolio/layouts/another.vue
+++ nuxt-blog/portfolio/layouts/another.vue
@@ -3,4 +3,7 @@
         <div>This is another layout</div>
         <slot/>
     </div>
+    <div>
+        <NuxtLink to="/" class="bg-orange-500 text-white mt-4">Go back</NuxtLink>
+    </div>
 </template>
